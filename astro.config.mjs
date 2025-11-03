2eaf049252524bfffb532463cb6b6b3b0a2ee410
Platte <gruberplatte@gmail.com>
2025-11-03 15:07:27 -0600

    fix build;2;13~

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
-import { markdownConfigDefaults } from '@astrojs/markdown-remark';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-  markdown: {
-    ...markdownConfigDefaults,
-  },
 });

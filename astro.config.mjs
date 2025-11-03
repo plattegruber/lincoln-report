66c6a3b565b337b8f0e3bc773183223f02db5053
plattegruber <gruberplatte@gmail.com>
2025-11-02 22:55:11 -0600

    Remove unused markdown config dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
-import { markdownConfigDefaults } from '@astrojs/markdown-remark';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-  markdown: {
-    ...markdownConfigDefaults,
-  },
 });
